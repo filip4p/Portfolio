3b9425224a3b1afda7f180b10b378539ca8ccc4c
filip4p <filip4p@gmail.com>
2023-06-30 22:59:28 +0200

    30.06.2023 page update

--- server.py
+++ server.py
@@ -8,5 +8,15 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/1")
+def open_first_jpg():
+    return send_file(path_or_file='static/img/Podajnik wibracyjny.png', as_attachment=False)
+
+
+@app.route("/download_cv")
+def download_cv():
+    return send_file(path_or_file='static/img/CV Prorok Filip.pdf', as_attachment=True)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
